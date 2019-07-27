1c0c96cb8a6b9b4fb93baf40a356a8449f4bf6f8
Ikhtiar <ikhtiaralam@gmail.com>
2019-07-27 10:21:07 -0500

    autoloop function

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -10,7 +10,7 @@ class Jumbotron extends Component {
   }
   render() {
     return (
-      <video id="background-video" loop autoPlay>
+      <video id="background-video" loop autoPlay muted>
         <source src={this.state.videoURL} type="video/mp4" />
         <source src={this.state.videoURL} type="video/ogg" />
         Your browser does not support the video tag.
